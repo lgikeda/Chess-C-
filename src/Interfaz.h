6e5f720a3104e770c2e5e1ae2852960d47791215
Diego Manzano <127451491+diegomanzanoggg@users.noreply.github.com>
2023-05-21 23:27:03 +0200

    Add files via upload
    clase interfaz para controlar los menus y estados del juego
    
    Signed-off-by: Diego Manzano <127451491+diegomanzanoggg@users.noreply.github.com>

---
+++ src/Interfaz.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Entorno.h"
+#include "freeglut.h"
+#include "ETSIDI.h"
+
+
+class Interfaz
+{
+public: 
+	Interfaz();
+	virtual ~Interfaz();
+
+	void tecla(unsigned char key);
+	//void mueve();																//debemos añadirla mas adelante
+	void dibuja();
+	void raton(int button, int state, int x, int y);
+
+protected: 
+	Entorno entorno;
+	enum Estado {INICIO, SEL_EJERCITO, JUEGO, FIN};
+	Estado estado;
+};
+
